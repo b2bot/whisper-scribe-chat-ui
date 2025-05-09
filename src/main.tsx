e04d406f3458e6a0d66c4d13ccf0a6d86cb56a64
b2bot <vagner@leadclinic.com.br>
2025-05-09 12:34:19 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,4 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Forçar tema claro (remove classe "dark" e aplica "light")
-document.documentElement.classList.remove('dark')
-document.documentElement.classList.add('light')
-
 createRoot(document.getElementById("root")!).render(<App />);
